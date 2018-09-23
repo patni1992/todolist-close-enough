04b67712a233dd08d7082c2dd5ef3e07990dd311
patni1992 <patriknilsson1992@gmail.com>
2018-09-24 01:14:50 +0200

    can now addTodo

--- src/components/todolist/Addtodo.vue
+++ src/components/todolist/Addtodo.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="add-todo">
-    <h1>Add-todo {{msg}}</h1>
-    <b-form>
+    <h1>Add todo</h1>
+    <b-form @submit.prevent="addTodo">
         <b-form-group id="exampleInputGroup1">
-            <b-form-input id="exampleInput1" type="text" required placeholder="Enter todo">
+            <b-form-input id="exampleInput1" type="text"   v-model="title"  required placeholder="Enter todo">
             </b-form-input>
         </b-form-group>
         <b-button type="submit" variant="success">Submit</b-button>
@@ -12,7 +12,28 @@
 </template>
 
 <script>
+import { ADD_TODO_MUTATION, ALL_TODOS_QUERY } from "@/graphql";
 export default {
-  name: "AddTodo"
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.$apollo.mutate({
+        mutation: ADD_TODO_MUTATION,
+        variables: {
+          title: this.title
+        },
+        update: (store, { data: { createTodo } }) => {
+          const data = store.readQuery({ query: ALL_TODOS_QUERY });
+          data.allTodos.unshift(createTodo);
+          store.writeQuery({ query: ALL_TODOS_QUERY, data });
+        }
+      });
+    }
+  }
 };
 </script>
