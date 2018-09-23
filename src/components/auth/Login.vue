06fe68c04da671422017bdd835e23b67b61a99e3
patni1992 <patriknilsson1992@gmail.com>
2018-09-24 01:13:14 +0200

    implement login component

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="login">
-    <b-form>
+    <b-form  method="POST" @submit.prevent="login">
         <b-form-group id="exampleInputGroup1">
-            <b-form-input id="exampleInput1" type="email" required placeholder="Enter email">
+            <b-form-input v-model="password" id="exampleInput1" type="email" required placeholder="Enter email">
             </b-form-input>
         </b-form-group>
         <b-form-group id="exampleInputGroup2">
-            <b-form-input id="exampleInput2" type="password" required placeholder="Enter password">
+            <b-form-input v-model="email" id="exampleInput2" type="password" required placeholder="Enter password">
             </b-form-input>
         </b-form-group>
         <b-button type="submit" variant="success">Submit</b-button>
@@ -15,11 +15,31 @@
 </template>
 
 <script>
+import { LOGIN_MUTATION } from "@/graphql";
+
 export default {
   name: "Login",
   data() {
-    return {};
+    return {
+      email: "",
+      password: ""
+    };
   },
-  methods: {}
+  methods: {
+    login() {
+      this.$apollo
+        .mutate({
+          mutation: LOGIN_MUTATION,
+          variables: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        .then(response => {
+          localStorage.setItem("jwt-token", response.data.login.token);
+          this.$router.replace("/");
+        });
+    }
+  }
 };
 </script>
