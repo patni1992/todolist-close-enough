6809ca06e940b9c1901184c0f0fd1c4774ce16d6
patni1992 <patriknilsson1992@gmail.com>
2018-09-24 01:12:56 +0200

    setup apollo

---
+++ config.js
@@ -0,0 +1,2 @@
+export const graphqlUri =
+  process.env.NODE_ENV === "production" ? "" : "https://fakerql.com/graphql";
--- src/main.js
+++ src/main.js
@@ -1,14 +1,52 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
+import { graphqlUri } from "../config.js";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+const httpLink = new HttpLink({
+  uri: graphqlUri
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("jwt-token");
+  const setHeaders = {
+    ...headers
+  };
+
+  if (token) {
+    setHeaders.authorization = `Bearer ${token}`;
+  }
+  return {
+    headers: {
+      ...setHeaders
+    }
+  };
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
