3e501580c0e43b1ea5f744cce6e4370c700500ff
patni1992 <patriknilsson1992@gmail.com>
2018-09-24 01:20:02 +0200

    remove unused css

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,4 @@ body {
   margin-top: 20px;
   color: $white;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
